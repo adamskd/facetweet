6da76d5890e6fa4edaaa11ba030b709307ff6355
adamskd <adamskristopher@ymail.com>
2023-03-27 16:10:45 -0500

    setup color, theme, dark mode and styling

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/icons-material";
+import styled from "@mui/icons-material";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+export default FlexBetween;
